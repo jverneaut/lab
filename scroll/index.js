08e272c65e9f50dd95ad71ad0b88bf9098ae38af
jverneaut <jverneaut@gmail.com>
2019-07-18 17:23:44 +0200

    Add scroll example

---
+++ scroll/index.js
@@ -0,0 +1,27 @@
+const body = document.querySelector('body');
+const content = document.querySelector('.content');
+
+const webkitOverflowScrolling = document.querySelector('#webkit');
+const vhJavascript = document.querySelector('#vh');
+
+webkitOverflowScrolling.addEventListener('change', () => {
+  if (webkitOverflowScrolling.checked) {
+    content.style.WebkitOverflowScrolling = 'touch';
+  } else {
+    content.style.WebkitOverflowScrolling = '';
+  }
+});
+
+const setHeight = () => {
+  body.style.height = window.innerHeight + 'px';
+}
+
+vhJavascript.addEventListener('change', () => {
+  if (vhJavascript.checked) {
+    setHeight();
+    window.addEventListener('resize', setHeight);
+  } else {
+    body.style.height = '';
+    window.removeEventListener('resize', setHeight);
+  }
+});
