f291cfdc6308dc7352deb6236cda4baade124f89
Julien Verneaut <jverneaut@gmail.com>
2019-10-23 10:51:11 +0200

    Add horizontal scroll demo

---
+++ horizontal-scroll/index.js
@@ -0,0 +1,39 @@
+const itemsContainer = document.querySelector('.items');
+const items = document.querySelectorAll('.item');
+const images = document.querySelectorAll('.item img');
+
+const RATIO = 0.12;
+
+let scroll = 0;
+let actualScroll = scroll;
+
+let containerWidth = itemsContainer.getBoundingClientRect().width;
+let windowWidth = window.innerWidth;
+
+window.onresize = () => {
+  containerWidth = itemsContainer.getBoundingClientRect().width;
+  windowWidth = window.innerWidth;
+};
+
+document.addEventListener('wheel', e => {
+  const delta = e.deltaX + e.deltaY;
+  actualScroll = clamp(actualScroll + delta, 0, containerWidth - windowWidth);
+});
+
+const clamp = (value, min, max) => Math.min(max, Math.max(value, min));
+
+const anim = () => {
+  scroll += (actualScroll - scroll) * RATIO;
+
+  itemsContainer.style.transform = `translate3d(${-scroll}px, 0, 0)`;
+
+  const normalizedScroll = scroll / (containerWidth - windowWidth);
+  images.forEach((image, index) => {
+    const normalizedOffset = normalizedScroll - (index + 0.5) / images.length;
+    image.style.transform = `translate3d(calc(-50% + ${normalizedOffset * 300}px), -50%, 0)`;
+  });
+
+  requestAnimationFrame(anim);
+};
+
+requestAnimationFrame(anim);
